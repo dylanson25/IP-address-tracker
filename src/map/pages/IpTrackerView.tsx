b49fe9e49b04e96e97d4c5e5093b3d0471f362c3
dylanson25 <dvillarreal0@ucol.mx>
2024-04-17 17:51:56 -0700

    Sass and css by modules

--- src/map/pages/IpTrackerView.tsx
+++ src/map/pages/IpTrackerView.tsx
@@ -1,16 +1,19 @@
+import styles from "../assets/IpTrackerView.module.sass";
+
 const IpTrackerView = () => {
   return (
-    <>
-      IP Address Tracker Search for any IP address or domain IP Address Location
-      Timezone UTC ISP
-      <div className="attribution">
-        Challenge by{" "}
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-          Frontend Mentor
-        </a>
-        . Coded by <a href="#">Your Name Here</a>.
-      </div>
-    </>
+    <div className={styles.container}>
+      <h1>IP Address Tracker</h1>
+    </div>
   );
 };
 export default IpTrackerView;
+//   IP Address Tracker Search for any IP address or domain IP Address Location
+//   Timezone UTC ISP
+//   <div className="attribution">
+//     Challenge by{" "}
+//     <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+//       Frontend Mentor
+//     </a>
+//     . Coded by <a href="#">Your Name Here</a>.
+//   </div>
