0a576cd8ede6278abf5e775fede4356af368c074
dylanson25 <dvillarreal0@ucol.mx>
2024-04-17 19:01:40 -0700

    Initial map

--- src/map/pages/IpTrackerView.tsx
+++ src/map/pages/IpTrackerView.tsx
@@ -1,9 +1,44 @@
 import styles from "../assets/IpTrackerView.module.sass";
 
+import { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
 const IpTrackerView = () => {
+  const [ipAddress, setIpAddress] = useState("");
+  useEffect(() => {
+    const fetchIp = async () => {
+      try {
+        const response = await fetch("https://api.ipify.org?format=json");
+        const data = await response.json();
+        setIpAddress(data.ip);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchIp();
+  }, []);
   return (
     <div className={styles.container}>
-      <h1>IP Address Tracker</h1>
+      <div className={styles.header}>
+        <h1>IP Address Tracker</h1>
+        <h2>{ipAddress}</h2>
+      </div>
+      <MapContainer
+        className={styles.leaftContainer}
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 };
